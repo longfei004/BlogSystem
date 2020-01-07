ab29cbbb1f22bc4176665d1072c009fd72b366ce
longfei.liu <longfei.liu@thoughtworks.com>
2020-01-07 16:24:37 +0800

    remove unused namespace.

--- BlogSystem.Portal.Tests/BlogsControllerTests.cs
+++ BlogSystem.Portal.Tests/BlogsControllerTests.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlogSystem.Business;
-using BlogSystem.Portal;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
