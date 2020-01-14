39431d46a15620f8bb0a8b87e154a20f2eb0999e
longfei.liu <longfei.liu@thoughtworks.com>
2020-01-14 13:23:41 +0800

    ad just blog detail view.

--- BlogSystemApp/src/BlogManage/components/BlogContent/BlogContent.js
+++ BlogSystemApp/src/BlogManage/components/BlogContent/BlogContent.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import UserLogo from '../../../assets/avatar.jpg';
 import './BlogContent.less';
 
 const BlogContent = ({ blog }) => {
     return (
-        <div className='blog-detail'>
-            <p className='blog-title'>{blog.title}</p>
-            <hr />
-            <p className='blog-content'>{blog.content}</p>
+        <div className='blog-view'>
+            <p className='blog-view-title'>{blog.title}</p>
+            <div className='blog-view-user'>
+                <img className='user-logo-view' src={UserLogo} alt='This image is not exist!'/>
+                <div className='blog-info'>
+                    <p className='name-info'>刘龙飞</p>
+                    <p className='last-edit-date'>2020.01.01 20:00:00</p>
+                </div>
+            </div>
+            <p className='blog-view-content'>{blog.content}</p>
         </div>
     )
 }
